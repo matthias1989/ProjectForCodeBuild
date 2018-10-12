1a26409abcda37f12102acde4619ee0d9641dabe
matthias <matthias@DESKTOP-NR1EOD1>
2018-10-12 16:04:29 +0200

    First commit for CodeBuild

---
+++ src/nl/cavero/HelloApp.java
@@ -0,0 +1,15 @@
+package nl.cavero;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		Integer result = sum(2,3);
+		System.out.println("Hello world: "+result);
+
+	}
+	
+	public static Integer sum(Integer i1, Integer i2) {
+		return i1 + i2;
+	}
+
+}
---
+++ src/nl/cavero/HelloAppTest.java
@@ -0,0 +1,20 @@
+package nl.cavero;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class HelloAppTest {
+
+	@Before
+	public void beforeMethod() {
+		
+	}
+	
+	@Test
+	public void testMethod() {
+		Integer result = HelloApp.sum(2, 3);
+		Assert.assertEquals(new Integer(5), result);
+	}
+}
