25d5d4c94a00b7315baa883c22719fbc3755a44a
matthias <matthias@DESKTOP-NR1EOD1>
2018-10-16 18:04:33 +0200

    test 37

--- src/main/java/nl/cavero/HelloApp.java
+++ src/main/java/nl/cavero/HelloApp.java
@@ -4,7 +4,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		Integer result = sum(2,10);
-		System.out.println("Hello world - 36 : "+result);
+		System.out.println("Hello world - 37 : "+result);
 
 	}
 	
